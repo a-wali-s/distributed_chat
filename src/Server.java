85e58e8b5c09d96c80a2722e436da716c225324e
Dominic Renaud <dvr@sfu.ca>
2012-02-11 14:44:37 -0800

    * First attempt at multithreaded server setup

--- src/Server.java
+++ src/Server.java
@@ -8,7 +8,6 @@ import java.net.Socket;
 public class Server implements Runnable{
 	
 	ServerSocket providerSocket;
-	Socket connection = null;
 	ObjectOutputStream out;
 	ObjectInputStream in;
 	String message;
@@ -16,61 +15,20 @@ public class Server implements Runnable{
 	
 	public void run()
 	{
-		try{
+		try {
 			//1. creating a server socket
 			providerSocket = new ServerSocket(2004, 10);
 			//2. Wait for connection
 			System.out.println("Waiting for connection");
-			connection = providerSocket.accept();
-			System.out.println("Connection received from " + connection.getInetAddress().getHostName());
-			//3. get Input and Output streams
-			out = new ObjectOutputStream(connection.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(connection.getInputStream());
-			sendMessage("Connection successful");
-			//4. The two parts communicate via the input and output streams
-			do{
-				try{
-					message = (String)in.readObject();
-					System.out.println("client>" + message);
-					if (message.equals("bye"))
-						sendMessage("bye");
-					else{
-						message = "Server receipted message: " + message;
-						sendMessage(message);
-					}
-				}
-				catch(ClassNotFoundException classnot){
-					System.err.println("Data received in unknown format");
-				}
-			}while(!message.equals("bye"));
+			
+			ServerThread newConnection = new ServerThread(providerSocket.accept(), providerSocket);
+			new Thread(newConnection).start();
 		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-		finally{
-			//4: Closing connection
-			try{
-				in.close();
-				out.close();
-				providerSocket.close();
-			}
-			catch(IOException ioException){
-				ioException.printStackTrace();
-			}
+		catch (IOException e){
+			e.printStackTrace();
 		}
 	}
-	void sendMessage(String msg)
-	{
-		try{
-			out.writeObject(msg);
-			out.flush();
-			System.out.println("server>" + msg);
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-	}
+			
 	public static void main(String args[])
 	{
 		Server srvr = new Server();
--- src/main.java
+++ src/main.java
@@ -5,8 +5,10 @@ public class main {
 	
 	public static void main(String[] args) {
 		Thread client = new Thread(new Client(),"T1");
+		Thread client2 = new Thread(new Client(),"T15");
 		Thread server = new Thread(new Server(),"T2");
 	    client.start();
+	    client2.start();
 	    server.start();
         System.out.println("Goodbye, World");
     }
