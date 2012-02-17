d8971950de0c1b95b35370c8046b3eb25ef2332d
Dominic Renaud <dvr@sfu.ca>
2012-02-17 13:20:55 -0800

    * Added Message class and cleaned up ClientInterface class

--- src/application/ClientInterface.java
+++ src/application/ClientInterface.java
@@ -1,67 +1,12 @@
 package application;
 
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.ListIterator;
-public class ClientInterface implements Runnable{
-	Socket requestSocket;
-	ObjectOutputStream out;
- 	ObjectInputStream in;
- 	BufferedReader usrIn;
+public class ClientInterface{
  	ArrayList<Connection> connections;
  	String message;
 	public ClientInterface(){
 		connections = new ArrayList<Connection>();
-	}
-	public void run()
-	{
-//		try{
-//			//1. creating a socket to connect to the server
-//			requestSocket = new Socket("localhost", 2004);
-//			System.out.println("Connected to localhost in port 2004");
-//			//2. get Input and Output streams
-//			out = new ObjectOutputStream(requestSocket.getOutputStream());
-//			out.flush();
-//			in = new ObjectInputStream(requestSocket.getInputStream());
-//			usrIn = new BufferedReader(new InputStreamReader(System.in));
-//			//3: Communicating with the server
-//			do{
-//				try{
-//					message = (String)in.readObject();
-//					System.out.println("server>" + message);
-//					message = usrIn.readLine();
-//					sendMessage(message);
-//				}
-//				catch(ClassNotFoundException classNot){
-//					System.err.println("data received in unknown format");
-//				}
-//			}while(!message.equals("bye"));
-//		}
-//		catch(UnknownHostException unknownHost){
-//			System.err.println("You are trying to connect to an unknown host!");
-//		}
-//		catch(IOException ioException){
-//			ioException.printStackTrace();
-//		}
-//		finally{
-//			//4: Closing connection
-//			try{
-//				in.close();
-//				out.close();
-//				requestSocket.close();
-//			}
-//			catch(IOException ioException){
-//				ioException.printStackTrace();
-//			}
-//		}
 	}
 	void addConnection(Connection conn){
 		connections.add(conn);
@@ -78,9 +23,4 @@ public class ClientInterface implements Runnable{
 	{
 		//TODO: Send message to messageAPI
 	}
-//	public static void main(String args[])
-//	{
-//		Client cli = new Client();
-//		cli.run();
-//	}
 }
