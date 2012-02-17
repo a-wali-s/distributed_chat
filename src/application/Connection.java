55acc39a6ff27a62099dd53529607b2c5c0129fb
Dominic Renaud <dvr@sfu.ca>
2012-02-17 11:20:34 -0800

    * Added sendMessage and receiveMessage methods to Connection class

--- src/application/Connection.java
+++ src/application/Connection.java
@@ -7,10 +7,34 @@ public class Connection {
 	public Socket socket = null;
 	public ObjectOutputStream out = null;
 	public ObjectInputStream in = null;
+	String inBuffer = null;
 	
 	public Connection(Socket socket) throws IOException{
 		this.socket = socket;
 		out = new ObjectOutputStream(socket.getOutputStream());
 		in = new ObjectInputStream(socket.getInputStream());
+	}
+	
+	public int sendMessage(String msg) {
+		try {
+			out.writeObject(msg);
+			return 0;
+		}
+		catch (IOException e) {
+			return -1;
+		}
+	}
+	
+	public String receiveMessage() {
+		try {
+			inBuffer = in.readObject().toString();
+			return String.copyValueOf(inBuffer.toCharArray());
+		}
+		catch(IOException e){
+			return "-1";
+		}
+		catch(ClassNotFoundException e){
+			return "String not received";
+		}
 	}
 }
