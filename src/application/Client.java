75b74b9fd94cfbcf51ca8da63bdca64e4897712d
Dominic Renaud <dvr@sfu.ca>
2012-02-17 11:35:23 -0800

    * Added addConnection and receiveMessage method to Client

--- src/application/Client.java
+++ src/application/Client.java
@@ -9,68 +9,79 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.ListIterator;
 public class Client implements Runnable{
 	Socket requestSocket;
 	ObjectOutputStream out;
  	ObjectInputStream in;
  	BufferedReader usrIn;
+ 	ArrayList<Connection> connections;
  	String message;
-	public Client(){}
+	public Client(){
+		connections = new ArrayList<Connection>();
+	}
 	public void run()
 	{
-		try{
-			//1. creating a socket to connect to the server
-			requestSocket = new Socket("localhost", 2004);
-			System.out.println("Connected to localhost in port 2004");
-			//2. get Input and Output streams
-			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(requestSocket.getInputStream());
-			usrIn = new BufferedReader(new InputStreamReader(System.in));
-			//3: Communicating with the server
-			do{
-				try{
-					message = (String)in.readObject();
-					System.out.println("server>" + message);
-					message = usrIn.readLine();
-					sendMessage(message);
-				}
-				catch(ClassNotFoundException classNot){
-					System.err.println("data received in unknown format");
-				}
-			}while(!message.equals("bye"));
-		}
-		catch(UnknownHostException unknownHost){
-			System.err.println("You are trying to connect to an unknown host!");
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-		finally{
-			//4: Closing connection
-			try{
-				in.close();
-				out.close();
-				requestSocket.close();
-			}
-			catch(IOException ioException){
-				ioException.printStackTrace();
-			}
-		}
+//		try{
+//			//1. creating a socket to connect to the server
+//			requestSocket = new Socket("localhost", 2004);
+//			System.out.println("Connected to localhost in port 2004");
+//			//2. get Input and Output streams
+//			out = new ObjectOutputStream(requestSocket.getOutputStream());
+//			out.flush();
+//			in = new ObjectInputStream(requestSocket.getInputStream());
+//			usrIn = new BufferedReader(new InputStreamReader(System.in));
+//			//3: Communicating with the server
+//			do{
+//				try{
+//					message = (String)in.readObject();
+//					System.out.println("server>" + message);
+//					message = usrIn.readLine();
+//					sendMessage(message);
+//				}
+//				catch(ClassNotFoundException classNot){
+//					System.err.println("data received in unknown format");
+//				}
+//			}while(!message.equals("bye"));
+//		}
+//		catch(UnknownHostException unknownHost){
+//			System.err.println("You are trying to connect to an unknown host!");
+//		}
+//		catch(IOException ioException){
+//			ioException.printStackTrace();
+//		}
+//		finally{
+//			//4: Closing connection
+//			try{
+//				in.close();
+//				out.close();
+//				requestSocket.close();
+//			}
+//			catch(IOException ioException){
+//				ioException.printStackTrace();
+//			}
+//		}
+	}
+	void addConnection(Connection conn){
+		connections.add(conn);
 	}
 	void sendMessage(String msg)
 	{
-		try{
-			out.writeObject(msg);
-			out.flush();
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
+		if (connections.size() > 0){
+			for(int x=0;x<connections.size();x++){
+				connections.get(x).sendMessage(msg);
+			}
 		}
 	}
-	public static void main(String args[])
+	String receiveMessage(String msg)
 	{
-		Client cli = new Client();
-		cli.run();
+		
+		return "";
 	}
+//	public static void main(String args[])
+//	{
+//		Client cli = new Client();
+//		cli.run();
+//	}
 }
