791a623bac39546e62ae90d2f40f7a7b50e17a9e
Dominic Renaud <dvr@sfu.ca>
2012-02-17 13:22:49 -0800

    * Cleaned up ConnectionListener class

--- src/application/ConnectionListener.java
+++ src/application/ConnectionListener.java
@@ -2,16 +2,12 @@ package application;
 
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class ConnectionListener implements Runnable{
 	
 	ServerSocket providerSocket;
 	Socket connection = null;
-//	ObjectOutputStream out;
-//	ObjectInputStream in;
 	String message;
 	ClientInterface client;
 	public ConnectionListener(ClientInterface client){
@@ -28,28 +24,11 @@ public class ConnectionListener implements Runnable{
 				System.out.println("Waiting for connection");
 				connection = providerSocket.accept();
 				System.out.println("Connection received from " + connection.getInetAddress().getHostName());
-				//3. get Input and Output streams
+				//3. Wrap in a connection object, spawn a thread, and go back to listening
 				Connection connWrapper = new Connection(connection, client);
 				Thread connThread = new Thread(connWrapper);
 				connThread.start();
 				client.addConnection(connWrapper);
-//			sendMessage("Connection successful");
-			//4. The two parts communicate via the input and output streams
-//			do{
-//				try{
-//					message = (String)in.readObject();
-//					System.out.println("client>" + message);
-//					if (message.equals("bye"))
-//						sendMessage("bye");
-//					else{
-//						MessageHandler.getInstance().reseiveMsg(message);
-//						sendMessage(message);
-//					}
-//				}
-//				catch(ClassNotFoundException classnot){
-//					System.err.println("Data received in unknown format");
-//				}
-//			}while(!message.equals("bye"));
 			}
 		}
 		catch(IOException ioException){
@@ -58,8 +37,6 @@ public class ConnectionListener implements Runnable{
 		finally{
 			//4: Closing connection
 			try{
-//				in.close();
-//				out.close();
 				providerSocket.close();
 			}
 			catch(IOException ioException){
@@ -67,23 +44,5 @@ public class ConnectionListener implements Runnable{
 			}
 		}
 	}
-//	void sendMessage(String msg)
-//	{
-//		try{
-//			out.writeObject(msg);
-//			out.flush();
-//			System.out.println("server>" + msg);
-//		}
-//		catch(IOException ioException){
-//			ioException.printStackTrace();
-//		}
-//	}
-//	public static void main(String args[])
-//	{
-//		Server srvr = new Server();
-//		while(true){
-//			srvr.run();
-//		}
-//	}
 }
 
